1cebc348b88cdeb13abf71bec6688009a161c3dd
Chepman32 <antonkerch555@gmail.com>
2024-02-11 19:55:09 +0300

    SelectedAuctionDetails

--- src/pages/AuctionPage/SelectedAuctionDetails.jsx
+++ src/pages/AuctionPage/SelectedAuctionDetails.jsx
@@ -15,32 +15,26 @@ export const SelectedAuctionDetails = ({ selectedAuction }) => {
             title={<h3>{`${selectedAuction.make.toUpperCase()} ${selectedAuction.model}`} </h3>}
             style={{ flex: 1, display: 'flex', flexDirection: 'column' }}
           >
-            <Space direction="vertical" style={{ flex: 1 }}>
-              <div style={{ flex: 1, overflow: 'hidden' }}>
-                <img
-                  src={getImageSource(selectedAuction.make, selectedAuction.model)}
-                  alt="Auction"
-                  className="auctionDetails_image"
-                />
-              </div>
-              <Typography.Text className="subText" onClick={() => console.log(selectedAuction)} >{selectedAuction?.lastBidPlayer}</Typography.Text>
-              <Flex direction="column" justify="space-between" align="center" style={{ flex: 1 }}>
-              <Typography.Text className="subText">
-                    {`${selectedAuction?.currentBid ? "Current" : "Start"} Bid: `}
-                  </Typography.Text>
-                  <Typography.Text className="price bid">
-                    &nbsp;{selectedAuction?.currentBid || selectedAuction.minBid}
-                  </Typography.Text>
-              </Flex>
-              <Flex direction="column" justify="space-between" align="center" style={{ flex: 1 }}>
-              <Typography.Text className="subText">
-                    {`Buy out:`}
-                  </Typography.Text>
-                  <Typography.Text className="price buy">
-                    {`${selectedAuction.buy}`}
-                  </Typography.Text>
-              </Flex>
-            </Space>
+            <div style={{ overflow: 'hidden' }}>
+              <img
+                src={getImageSource(selectedAuction.make, selectedAuction.model)}
+                alt="Auction"
+                className="auctionDetails_image"
+              />
+            </div>
+            <Flex direction="column" align="center" style={{ marginTop: '20px', minWidth: "100%", justifyContent: "space-between" }}>
+              <Typography.Text className="subText">{selectedAuction?.lastBidPlayer}</Typography.Text>
+              <Space direction="vertical" style={{width: "50%",}}>
+                <div style={{width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                  <Typography.Text className="subText">{`${selectedAuction?.currentBid ? "Current" : "Start"} Bid:`}&nbsp;</Typography.Text>
+                  <Typography.Text className="price bid">{selectedAuction?.currentBid || selectedAuction.minBid}</Typography.Text>
+                </div>
+                <Space>
+                  <Typography.Text className="subText">Buy out:</Typography.Text>
+                  <Typography.Text className="price buy">{selectedAuction.buy}</Typography.Text>
+                </Space>
+              </Space>
+            </Flex>
           </Card>
         </Flex>
       )}
